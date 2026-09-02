f43e10e5f2dc1997593b482ae99c4dc6a0c8bac3
AmanDwivedi2324 <0786ad2000@gmail.com>
2026-09-02 23:16:37 +0530

    MVP DONE

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
          "http://localhost:5173",
     "http://localhost:3000",
-    "https://aman-ai-portfolio-backend.onrender.com/",
+    "https://aman-ai-portfolio.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
